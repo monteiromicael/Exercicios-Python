2c5148e1d9833e1ebd09f88b9bb6d757ccd7d78c
Micael <souzamicaelribeiro@gmail.com>
2021-09-16 18:51:56 -0300

    correção

--- Chute_o_Numero/chute.py
+++ Chute_o_Numero/chute.py
@@ -1,7 +1,18 @@
 import random
-import tratamento
 
-tratamento.Tratamento(int(input('Chute um numero')), random.randrange(1, 100))
+aleatorio = random.randrange(1, 100)
+palpite = int(input('Chute um numero! '))
 
-tratamento.maior()
 
+while True:
+    if palpite > aleatorio:
+        print('Chute um Valor menor!')
+        palpite = int(input('Chute um numero! '))
+
+    elif palpite < aleatorio:
+        print("Chute um Valor Maior! ")
+        palpite = int(input('Chute um numero! '))
+
+    elif palpite == aleatorio:
+        print("Parabens Você Acertou!!")
+        break
